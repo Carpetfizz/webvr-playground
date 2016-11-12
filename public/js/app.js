31d8e61053bfb0fc2e5b0e6a7a0eda4ede512f74
Nihar Mauskar <nihar.mauskar@gmail.com>
2016-11-12 04:41:26 -0800

    added comments about how to upload stuff from clara.io onto threejs

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,23 @@ document.getElementById("canvas-container").appendChild( renderer.domElement );
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
+
+//For objects
+/*
+var ObjLoader = new THREE.ObjectLoader();
+ObjLoader.load("dinosaur.json", function(obj){
+	obj.material.color.setHex( 0xff0000 );
+	scene.add( obj );
+});
+*/
+//For JSONS
+/*
+var JSONLoader = new THREE.JSONLoader();
+JSONLoader.load("dinosaur.json", function(geo, mats){
+	var obj = new THREE.Mesh( geo, new THREE.MeshBasicMaterial({color: 0xff0000}) );
+	scene.add( obj );
+});
+*/
 scene.add( cube );
 
 camera.position.z = 5;
