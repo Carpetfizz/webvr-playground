81237b63030e2bd4c06e73d3fdd4427e66ac0c14
Ajay Ramesh <ajayramesh.dev@gmail.com>
2016-11-12 23:21:21 -0800

    cleaned up viewer logic

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,57 +1,117 @@
 var io = io();
-var roomId = document.location.href.substring(document.location.href.lastIndexOf( '/' )+1);
+var canvas,
+    objects,
+    scene,
+    camera,
+    renderer,
+    stereo,
+    controls,
+    isMobile;
 
-io.emit('viewerjoin', {room: roomId});
+isMobile = false;
+if (/Mobi/.test(navigator.userAgent)) {
+    isMobile = true;
+}
 
-io.on('gameinput', function(input){
-    const mult = 0.12;
-	camera.position.x += input.x * mult;
+
+function setupGamepad(cb) {
+    var roomId = document.location.href.substring(document.location.href.lastIndexOf( '/' )+1);
+
+    io.emit('viewerjoin', {room: roomId});
+
+    io.on('gameinput', function(input){
+        console.log(input);
+        cb(input);
+    });
+}
+
+function updateCameraPosition(input) {
+    var mult = 0.12;
+    camera.position.x += input.x * mult;
     camera.position.y -= input.z * mult;
-});
+}
 
 
-const canvas = document.getElementById("canvas-container");
-var raycaster = new THREE.Raycaster();
-var objects = [];
-var selection = null;
-var offset = new THREE.Vector3();
-// Plane, that helps to determinate an intersection position
-var helper = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
-helper.visible = false;
+function setupScene() {
+    
+    canvas = document.getElementById("canvas-container");
+    objects = [];
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xadd8e6);
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer();
+    stereo = new THREE.StereoEffect(renderer);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById("canvas-container").appendChild( renderer.domElement );
 
 
-var scene = new THREE.Scene();
-scene.background = new THREE.Color(0xadd8e6);
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    /* BUILD OBJECTS */
+    // Plane, that helps to determinate an intersection position
+    var helperPlane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
+    helperPlane.visible = false;
+    var box_geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var box_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( box_geometry, box_material );
 
-var renderer = new THREE.WebGLRenderer();
-var stereo = new THREE.StereoEffect(renderer);
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.getElementById("canvas-container").appendChild( renderer.domElement );
+    var geometry = new THREE.PlaneGeometry( 100, 100, 100 );
+    var texture = new THREE.TextureLoader().load("/textures/cube1.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(10, 10);
+    var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+    var plane = new THREE.Mesh( geometry, material );
 
-var box_geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var box_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( box_geometry, box_material );
+    /* SHOW OBJECTS */
+    scene.add(helperPlane);
+    scene.add(plane);
 
-var box1_geometry = new THREE.BoxGeometry( 2, 1, 1 );
-var box1_material = new THREE.MeshBasicMaterial( { color: 0x01fde0 } );
-var cube1 = new THREE.Mesh( box1_geometry, box1_material );
+    displayObject(cube);
 
-var geometry = new THREE.PlaneGeometry( 100, 100, 100 );
-var texture = new THREE.TextureLoader().load("/textures/cube1.jpg");
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(10, 10);
-var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
+    camera.position.z = 10;
+    camera.position.y = -30;
+    camera.lookAt(scene.position);
+}
 
-scene.add(helper);
-scene.add(plane);
-displayObject(cube);
-displayObject(cube1);
-camera.position.z = 10;
-camera.position.y = -30;
-camera.lookAt(scene.position);
+function setupOrientationControls() {
+
+    window.addEventListener('deviceorientation', setOrientationControls, true);
+    controls = new THREE.DeviceOrientationControls(camera, renderer.domElement);
+    controls.connect();
+    function setOrientationControls(e){
+        if(!e.alpha){
+            return;
+        }
+        controls = new THREE.DeviceOrientationControls(camera, true);
+        controls.connect();
+        controls.update();
+        window.removeEventListener('deviceorientation', setOrientationControls, true);
+    }
+}
+
+function setupRenderer() {
+    
+    var render = function () {
+	    requestAnimationFrame( render );
+        if(isMobile){
+            controls.update();
+            stereo.render(scene, camera);
+        } else {
+            renderer.render(scene, camera);
+        }
+    };
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        stereo.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    render();
+}
 
 function displayObject(object) {
 	// display object json
@@ -61,22 +121,7 @@ function displayObject(object) {
     object.translateY(-20);
 }
 
-var render = function () {
-	requestAnimationFrame( render );
-    // renderer.render(scene, camera);
-    stereo.render(scene, camera);
-};
-
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    stereo.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-
-render();
+setupScene();
+setupOrientationControls();
+setupRenderer();
+setupGamepad(updateCameraPosition);
