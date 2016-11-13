5947b7e72af163c485ebe67f250e05b6ea15c7a5
Peter Wang <sheng-yu_wang@berkeley.edu>
2016-11-13 11:09:03 +0800

    editor modified

--- public/js/app.js
+++ public/js/app.js
@@ -1,142 +1,37 @@
-const canvas = document.getElementById("canvas-container");
-var raycaster = new THREE.Raycaster();
-var objects = [];
-var selection = null;
-var offset = new THREE.Vector3();
-// Plane, that helps to determinate an intersection position
-var helper = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
-helper.visible = false;
+var io = io();
 
+initgp(input);
 
-var scene = new THREE.Scene();
-scene.background = new THREE.Color(0xadd8e6);
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+io.emit('editorjoin', {room: roomId});
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.getElementById("canvas-container").appendChild( renderer.domElement );
-
-var box_geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var box_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( box_geometry, box_material );
-
-var box1_geometry = new THREE.BoxGeometry( 2, 1, 1 );
-var box1_material = new THREE.MeshBasicMaterial( { color: 0x01fde0 } );
-var cube1 = new THREE.Mesh( box1_geometry, box1_material );
-
-var geometry = new THREE.PlaneGeometry( 100, 100, 100 );
-var texture = new THREE.TextureLoader().load("textures/cube1.jpg");
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(10, 10);
-var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-
-scene.add(helper);
-scene.add(plane);
-displayObject(cube);
-displayObject(cube1);
-camera.position.z = 10;
-camera.position.y = -30;
-camera.lookAt(scene.position);
-
-// controls = new THREE.OrbitControls(camera, renderer.domElement);
-// controls.addEventListener( 'change', render );
-
-
-
-function displayObjectById(id) {
-	// get object json based on id and calls displayObject
-	
+function input(i) {
+    io.emit('sendinput', {input: i});
 }
 
-function displayObject(object) {
-	// display object json
-	objects.push(object);
-	scene.add(object);
-	object.translateZ(object.scale.z/2);
-}
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const zmin = document.getElementById("zoomin");
+const zmout = document.getElementById("zoomout");
+let menuOpen = true;
 
-var render = function () {
-	requestAnimationFrame( render );
-    renderer.render(scene, camera);
+
+function showObjectByID(i) {
+  displayObjectByID(i);
 };
 
-window.addEventListener( 'resize', onWindowResize, false );
+// zmin.onclick = zoomin;
+// zmout.onclick = zoomout;
 
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
+menuToggle.onclick = function() {
+  const openClass = "menu menu-open";
+  const closeClass = "menu menu-close";
+  if(menuOpen) {
+    menuOpen = false;
+    menu.className = closeClass;
+    menuToggle.textContent = ">>";
+  } else {
+    menuOpen = true;
+    menu.className = openClass;
+    menuToggle.textContent = "<<";
+  }
 }
-
-
-window.addEventListener( 'dblclick', onWindowDblclick, false );
-
-function onWindowDblclick() {
-	camera.position.z -= 1;
-}
-
-
-canvas.addEventListener('mousedown', this.onDocumentMouseDown, false);
-canvas.addEventListener('mousemove', this.onDocumentMouseMove, false);
-canvas.addEventListener('mouseup', this.onDocumentMouseUp, false);
-
-function onDocumentMouseDown(event) {
-	// Get mouse position
-	var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-	var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-	// Get 3D vector from 3D mouse position using 'unproject' function
-	var vector = new THREE.Vector3(mouseX, mouseY, 1);
-	vector.unproject(camera);
-	// Set the raycaster position
-	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
-	// Find all intersected objects
-	var intersects = raycaster.intersectObjects(objects);
-	if (intersects.length > 0) {
-		// Disable the controls
-		// controls.enabled = false;
-		// Set the selection - first intersected object
-		selection = intersects[0].object;
-		// Calculate the offset
-		var intersects = raycaster.intersectObject(helper);
-		offset.copy(selection.position).sub(helper.position);
-	}
-}
-
-function onDocumentMouseMove(event) {
-	event.preventDefault();
-	// Get mouse position
-	var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-	var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-	// Get 3D vector from 3D mouse position using 'unproject' function
-	var vector = new THREE.Vector3(mouseX, mouseY, 1);
-	vector.unproject(camera);
-	// Set the raycaster position
-	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
-	if (selection != null) {
-		// Check the position where the plane is intersected
-		var intersects = raycaster.intersectObject(plane);
-		// Reposition the object based on the intersection point with the plane
-		let tmp = selection.position.z;
-		selection.position.copy(intersects[0].point.sub(offset));
-		selection.translateZ(tmp);
-	} else {
-		// Update position of the plane if need
-		var intersects = raycaster.intersectObjects(objects);
-		if (intersects.length > 0) {
-			helper.position.copy(intersects[0].object.position);
-			helper.lookAt(camera.position);
-		}
-	}
-}
-
-function onDocumentMouseUp(event) {
-	// Enable the controls
-	// controls.enabled = true;
-	selection = null;
-}
-
-render();
--- public/js/editor.js
+++ public/js/editor.js
@@ -1,46 +1,219 @@
-var io = io();
+const canvas = document.getElementById("canvas-container");
+var raycaster = new THREE.Raycaster();
+var objects = [];
+var selection = null;
+var offset = new THREE.Vector3();
+var modify = null;
+// Plane, that helps to determinate an intersection position
+var helper = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
+helper.visible = false;
 
-initgp(input);
 
-io.emit('editorjoin', {room: roomId});
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xadd8e6);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-function input(i) {
-    io.emit('sendinput', {input: i});
-}
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.getElementById("canvas-container").appendChild( renderer.domElement );
 
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
-let menuOpen = true;
+var wtexture = new THREE.TextureLoader().load("textures/wood.jpg");
+wtexture.wrapS = THREE.RepeatWrapping;
+wtexture.wrapT = THREE.RepeatWrapping;
+//wtexture.repeat.set(10, 10);
+var w_material = new THREE.MeshBasicMaterial( { map: wtexture } );
 
-let inventorylst = [
-  {name: "Apple", id: "apple"},
-  {name: "Banana", id: "apple"},
-  {name: "Cat", id: "apple"},
-  {name: "Denny", id: "apple"},
-  {name: "Egg", id: "apple"},
-  {name: "Fruit", id: "apple"}
+var create_objects = [
+	function() {
+		let geometry = new THREE.BoxGeometry(3, 3, 3);
+		let obj = new THREE.Mesh(geometry, w_material);
+		return obj;
+	},
+
+	function() {
+		let geometry = new THREE.CylinderGeometry( 3, 3, 6, 32 );
+		let obj = new THREE.Mesh(geometry, w_material);
+		//obj.rotation.y += (Math.pi/2);
+		return obj;
+	},
+
+	function() {
+		let geometry = new THREE.BoxGeometry(10, 10, 10);
+		let obj = new THREE.Mesh(geometry, w_material);
+		return obj;
+	},
+
+	function() {
+		let geometry = new THREE.BoxGeometry(10, 10, 10);
+		let obj = new THREE.Mesh(geometry, w_material);
+		return obj;
+	},
+
+	function() {
+		let geometry = new THREE.BoxGeometry(10, 10, 10);
+		let obj = new THREE.Mesh(geometry, w_material);
+		return obj;
+	},
+
+	function() {
+		let geometry = new THREE.BoxGeometry(10, 10, 10);
+		let obj = new THREE.Mesh(geometry, w_material);
+		return obj;
+	},
 ];
 
-for (let inv of inventorylst) {
-  let btn = document.createElement("BUTTON");
-  let text = document.createTextNode(inv.name);
-  btn.appendChild(text);
-  // btn.onclick = displayObject(inv.object);
-  btn.className = "init_button";
-  menu.appendChild(btn);
+/*
+var box_geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var box_material = new THREE.MeshBasicMaterial( { map: wtexture } );
+var cube = new THREE.Mesh( box_geometry, box_material );
+
+var box1_geometry = new THREE.BoxGeometry( 2, 1, 1 );
+var box1_material = new THREE.MeshBasicMaterial( { map: wtexture} );
+var cube1 = new THREE.Mesh( box1_geometry, box1_material );
+*/
+
+var geometry = new THREE.PlaneGeometry( 100, 100, 100 );
+var texture = new THREE.TextureLoader().load("textures/cube1.jpg");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(10, 10);
+var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+var plane = new THREE.Mesh( geometry, material );
+
+scene.add(helper);
+scene.add(plane);
+// displayObject(cube);
+// displayObject(cube1);
+camera.position.z = 10;
+camera.position.y = -30;
+camera.lookAt(scene.position);
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener( 'change', render );
+
+
+
+function displayObjectByID(id) {
+	// get object json based on id and calls displayObject
+	displayObject(create_objects[id]());
+}
+
+function displayObject(object) {
+	// display object json
+	object.translateZ(object.geometry.parameters.height/2);
+	objects.push(object);
+	scene.add(object);
+}
+
+function removeObjectByID(id) {
+	// removeObject(cube);
+}
+
+function removeObject(object) {
+	objects.splice(objects.indexOf(object), 1);
+	scene.remove(object);
+}
+
+var render = function () {
+	requestAnimationFrame( render );
+    renderer.render(scene, camera);
+};
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+	update();
 }
 
 
-menuToggle.onclick = function() {
-  const openClass = "menu menu-open";
-  const closeClass = "menu menu-close";
-  if(menuOpen) {
-    menuOpen = false;
-    menu.className = closeClass;
-    menuToggle.textContent = ">>";
-  } else {
-    menuOpen = true;
-    menu.className = openClass;
-    menuToggle.textContent = "<<";
-  }
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
+
+/*
+window.addEventListener( 'dblclick', onWindowDblclick, false );
+
+function onWindowDblclick() {
+	camera.position.z -= 1;
+}
+*/
+
+
+canvas.addEventListener('mousedown', this.onDocumentMouseDown, false);
+canvas.addEventListener('mousemove', this.onDocumentMouseMove, false);
+canvas.addEventListener('mouseup', this.onDocumentMouseUp, false);
+
+function onDocumentMouseDown(event) {
+	// Get mouse position
+	var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+	var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+	// Get 3D vector from 3D mouse position using 'unproject' function
+	var vector = new THREE.Vector3(mouseX, mouseY, 1);
+	vector.unproject(camera);
+	// Set the raycaster position
+	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+	// Find all intersected objects
+	var intersects = raycaster.intersectObjects(objects);
+	if (intersects.length > 0 && intersects[0]) {
+		// Disable the controls
+		controls.enabled = false;
+		// Set the selection - first intersected object
+		selection = intersects[0].object;
+		// Calculate the offset
+		// console.log(intersects[0]);
+		var intersects = raycaster.intersectObject(helper);
+		offset.copy(intersects[0].point).sub(helper.position);
+	}
+}
+
+function onDocumentMouseMove(event) {
+	event.preventDefault();
+	// Get mouse position
+	var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+	var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+	// Get 3D vector from 3D mouse position using 'unproject' function
+	var vector = new THREE.Vector3(mouseX, mouseY, 1);
+	vector.unproject(camera);
+	// Set the raycaster position
+	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+	if (selection != null) {
+		// Check the position where the plane is intersected
+		var intersects = raycaster.intersectObject(plane);
+		// Reposition the object based on the intersection point with the plane
+		let tmp = selection.position.z;
+		selection.position.copy(intersects[0].point.sub(offset));
+		selection.translateZ(tmp);
+	} else {
+		// Update position of the plane if need
+		var intersects = raycaster.intersectObjects(objects);
+		if (intersects.length > 0) {
+			helper.position.copy(intersects[0].object.position);
+			helper.lookAt(camera.position);
+		}
+	}
+}
+
+function onDocumentMouseUp(event) {
+	// Enable the controls
+	controls.enabled = true;
+	selection = null;
+}
+/*
+function zoomout() {
+	camera.position.z += 1;
+	camera.position.y -= 3;
+}
+
+function zoomin() {
+	camera.position.z -= 1;
+	camera.position.y += 3;
+}
+*/
+
+render();
